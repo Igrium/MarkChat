bce32a0780b14140a7521003304b2e56d22df2b5
Sam54123 <thesam54123@gmail.com>
2023-12-10 14:46:26 -0800

    boilerplate

---
+++ src/client/java/com/igrium/markchat/MarkChatClient.java
@@ -0,0 +1,10 @@
+package com.igrium.markchat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MarkChatClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
